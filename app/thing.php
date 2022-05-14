28db3767e5bb9244aa232de41b41c996cb1e28d8
Rob Dirnens <rrdirnens@gmail.com>
2022-05-14 10:47:04 +0300

    change test directory back to app

---
+++ app/thing.php
@@ -0,0 +1,28 @@
+<?php 
+
+function function1(): bool
+{
+    $use = 1;
+    do {
+        echo "The number is: $use <br>";
+        $use++;
+    } while ($use <= 5);
+    for ($use = 0; $use <= 100; $use += 10) {
+        echo "The number is: $use <br>";
+    }
+    return false;
+}
+
+
+function function2(): bool
+{
+    $use = 1;
+    do {
+        echo "The number is: $use <br>";
+        $use++;
+    } while ($use <= 5);
+    for ($use = 0; $use <= 100; $use += 10) {
+        echo "The number is: $use <br>";
+    }
+    return false;
+}
