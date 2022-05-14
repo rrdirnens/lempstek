9af6f5360e8f81564b7f1d8c7248ee3c47d8c30b
Rob Dirnens <rrdirnens@gmail.com>
2022-05-14 10:43:24 +0300

    move duplicate code for testing

--- resources/views/travellist.blade.php
+++ resources/views/travellist.blade.php
@@ -18,5 +18,34 @@
                 <li>{{ $place->name }}</li>
           @endforeach
 	</ul>
+	@php
+		function function1(): bool
+		{
+			$use = 1;
+			do {
+				echo "The number is: $use <br>";
+				$use++;
+			} while ($use <= 5);
+			for ($use = 0; $use <= 100; $use += 10) {
+				echo "The number is: $use <br>";
+			}
+			return false;
+		}
+
+
+		function function2(): bool
+		{
+			$use = 1;
+			do {
+				echo "The number is: $use <br>";
+				$use++;
+			} while ($use <= 5);
+			for ($use = 0; $use <= 100; $use += 10) {
+				echo "The number is: $use <br>";
+			}
+			return false;
+		}
+
+	@endphp
 </body>
 </html>
