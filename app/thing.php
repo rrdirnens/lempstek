72c2b37166db652b5b785531644ff1125d0d9a83
Rob Dirnens <rrdirnens@gmail.com>
2022-05-14 10:27:20 +0300

    add thing

---
+++ app/thing.php
@@ -0,0 +1,31 @@
+/**
+* @return bool
+*/
+public function function1(): bool
+{
+    $use = 1;
+    do {
+        echo "The number is: $use <br>";
+        $use++;
+    } while ($use <= 5);
+    for ($use = 0; $use <= 100; $use += 10) {
+        echo "The number is: $use <br>";
+    }
+    return false;
+}
+
+/**
+* @return bool
+*/
+public function function2(): bool
+{
+    $use = 1;
+    do {
+        echo "The number is: $use <br>";
+        $use++;
+    } while ($use <= 5);
+    for ($use = 0; $use <= 100; $use += 10) {
+        echo "The number is: $use <br>";
+    }
+    return false;
+}
