8301e514fcaa6b06214bc1b7555f75404080fcbf
Rob Dirnens <rrdirnens@gmail.com>
2022-05-22 10:00:29 +0300

    delete garbage

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-
-
-// Route::get('/', function () {
-//   $visited = DB::select('select * from places where visited = ?', [1]);
-//   $togo = DB::select('select * from places where visited = ?', [0]);
-
-//   return view('travellist', ['visited' => $visited, 'togo' => $togo ] );
-// });
 
 Route::get('/', [Controller::class, 'home']);
